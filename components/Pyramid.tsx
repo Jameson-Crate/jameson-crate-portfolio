9df6a0101a3a75947fc5b7a1e109f33731f867d7
jameson-crate <jamesoncrate@gmail.com>
2024-09-09 21:08:51 -0700

    nah wtf

--- components/Pyramid.tsx
+++ components/Pyramid.tsx
@@ -1,8 +1,14 @@
-import React, { useRef } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { MeshWobbleMaterial } from '@react-three/drei';
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { MeshWobbleMaterial } from "@react-three/drei";
 
-const Pyramid = ({ color, position }: {color:any, position:any}) => {
+const Pyramid = ({
+  color,
+  position,
+}: {
+  color: string;
+  position: [number, number, number];
+}) => {
   const pyramidRef = useRef();
 
   // Animation loop: Rotate the pyramid
@@ -20,22 +26,29 @@ const Pyramid = ({ color, position }: {color:any, position:any}) => {
 
   return (
     <mesh ref={pyramidRef}>
-      <torusGeometry args={[ 2.78, 0.7, 120, 64]} />
+      <torusGeometry args={[2.78, 0.7, 120, 64]} />
       {/* <boxGeometry args={[4, 3, 3]} /> */}
-      <MeshWobbleMaterial attach="material" color={color} speed={2} factor={0.3} transparent opacity={0.35}/>
+      <MeshWobbleMaterial
+        attach="material"
+        color={color}
+        speed={2}
+        factor={0.3}
+        transparent
+        opacity={0.35}
+      />
     </mesh>
   );
 };
 
 const ThreePyramid = () => {
   return (
-    <div style={{ height: '70%', width: '100%', margin: '0 auto'}}>
+    <div style={{ height: "70%", width: "100%", margin: "0 auto" }}>
       <Canvas camera={{ position: [0, 0, 10], fov: 70 }}>
         <ambientLight intensity={1} />
         <directionalLight position={[0, 10, 5]} intensity={1} />
-        <Pyramid color={0xAF0000} position={[-5, -1, 0]}/>
-        <Pyramid color={0x0000AF} position={[5, -1, 0]}/>
-        <Pyramid color={0x00AF00} position={[0, 1, 0]}/>
+        <Pyramid color={0xaf0000} position={[-5, -1, 0]} />
+        <Pyramid color={0x0000af} position={[5, -1, 0]} />
+        <Pyramid color={0x00af00} position={[0, 1, 0]} />
       </Canvas>
     </div>
   );
